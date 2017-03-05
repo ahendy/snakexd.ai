778d1727c8a6a70213491be38a682771105400cd
ahendy0 <ahendy@hotmail.ca>
2017-03-04 17:56:03 -0800

    food heuristic

--- snake.py
+++ snake.py
@@ -74,7 +74,7 @@ def closest(x, y, state, board, depth):
             )
         )   
         x, y = closest['coords'][0]
-        return (me == closest['id']) * (1 + 20*(102-closest['health_points'])**2 + (board[y][x] == 'F') + (best(state, board, depth-1) if depth <= 0 else 0))
+        return (me == closest['id']) * (1 + (102-closest['health_points']) + (board[y][x] == 'F') + (best(state, board, depth-1) if depth <= 0 else 0))
 
 def update_board(state):
     snakes = state.snakes
